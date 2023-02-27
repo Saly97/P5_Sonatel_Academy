212c7bc7d5478888819be29a2538e9ab2c923d1a
Saly <salimatayayaba23@gmail.com>
2023-02-27 13:35:05 +0000

    Modif

--- 002_Algo-Python/affichage.py
+++
@@ -1,9 +0,0 @@
-from projet import* 
-fichier='/home/salimata/Bureau/algorithme/exoalgophython/projet.csv'
-tab_inv,tab_v=projet_donne(fichier)
-# affiche_valide_sans_notes(fichier)
-# affiche_invalide(fichier)
-#ex.affiche_valide_notes(fichier)
-#samamenu=ex.projet_donne(fichier)
-#menu(fichier)
-print(len(tab_v))
